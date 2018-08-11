10c89cbded6f592fd9596b9bc8ee0a82b070c1ac
Michael Anderson <Manderson@Pusheen.ayyy>
2018-08-10 20:23:10 -0700

    initial commit

---
+++ Installs/Scripts/WindowsSettings.ps1
@@ -0,0 +1,12 @@
+## Need to determine what kind of things we want here.
+
+<#
+Windows Update
+    Change Settings
+    Install Updates via PS
+
+    PS-ImportModule -Name .\Modules\PSWindowsUpdate -Verbose
+    
+#>
+
+
---
+++ Installs/StartHere.ps1
@@ -0,0 +1,10 @@
+## Written by Michael Anderson [michael@manderson.tech] ##
+Set-ExecutionPolicy Unrestricted -Scope Process 
+
+## This script will execute a number of other scripts, grouped into
+## application installs, Windows settings, and application settings.
+
+## Invoke-Expression $ScriptPath\Scripts\ApplicationInstall.ps1
+Invoke-Expression $ScriptPath\Scripts\WindowsSettings.ps1
+## Invoke-Expression $ScriptPath\Scripts\ApplicationSettings.ps1
+flkjhdzfxlkgjhdflkj
