1ba8c3241821e90bae1242ef99667cfb9d2fc8c0
Michael Anderson <Manderson@Pusheen.ayyy>
2018-08-19 12:52:07 -0700

    Modified StartHere to force install updated package managers and corrected other script calls (need to have .\ oops)

--- Installs/StartHere.ps1
+++ Installs/StartHere.ps1
@@ -1,10 +1,18 @@
 ## Written by Michael Anderson [michael@manderson.tech] ##
-Set-ExecutionPolicy Unrestricted -Scope Process 
 
 ## This script will execute a number of other scripts, grouped into
 ## application installs, Windows settings, and application settings.
 
+## Prep work
+
+## Allows this and child scripts to run unrestricted.
+Set-ExecutionPolicy Unrestricted -Scope Process
+
+## Installs updated package providers for later tasks (otherwise it prompts to update)
+Install-PackageProvider Nuget -Force
+Install-Module -Name PowerShellGet -Force
+
 ## Invoke-Expression $ScriptPath\Scripts\ApplicationInstall.ps1
-Invoke-Expression $ScriptPath\Scripts\WindowsSettings.ps1
-Invoke-Expression $ScriptPath\Scripts\ApplicationSettings.ps1
+Invoke-Expression .\Scripts\WindowsSettings.ps1
+## Invoke-Expression $ScriptPath\Scripts\ApplicationSettings.ps1
 ## lets try this again
