f6ab2eef23e9c3818b470536f54825366360f15e
Elliot Daniel <elliotdaniel.critical@gmail.com>
2018-09-17 23:01:47 -0700

    added folder options scaffolding

--- Installs/Scripts/WindowsSettings.ps1
+++ Installs/Scripts/WindowsSettings.ps1
@@ -5,8 +5,7 @@ Windows Update
     Change Settings  
 #>
 
-# Changing WU Settings
-
+## Windows Update settings
 # Set to "Notify to Schedule Restart". Default DWord Value is 0.
 Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings -Name UxOption -Type DWord -Value 1
 Write-Host "Windows Update UXOption set to 1 [Notify to Schedule Restart]."
@@ -15,3 +14,21 @@ Write-Host "Windows Update UXOption set to 1 [Notify to Schedule Restart]."
 Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings -Name ActiveHoursStart -Type DWord -Value 9
 Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings -Name ActiveHoursEnd -Type DWord -Value 1
 Write-Host "Active Hours set from 9AM to 1AM."
+## End Windows Update settings
+
+## Folder Options
+# Set default Explorer view to "This PC"
+
+# Show hidden files
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name Hidden -Type DWord -Value 1
+Write-Host "Enabled 'Show hidden files, folders and drives'."
+
+# Disable hide extensions for known file types
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name HideFileExt -Type DWord -Value 0
+Write-Host "Disabled 'Hide extensions for known file types'."
+## End Folder Options
+
+## Taskbar settings
+
+
+## End Taskbar settings
